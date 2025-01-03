f4953a5bd7b84adfcfccf107d42a069ada15ce58
iaghidro <iaghidro@gmail.com>
2025-01-03 15:16:33 -0500

    rename simple chat agent

---
+++ simple-chat-agent.py
@@ -0,0 +1,30 @@
+import os
+from autogen import AssistantAgent, UserProxyAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = "gpt-4o-mini"
+llm_config = {
+    "model": model,
+    "api_key": os.environ.get("OPENAI_API_KEY"),
+}
+
+
+assistant = AssistantAgent(
+    name="Assistant",
+    llm_config=llm_config,
+)
+
+user_proxy = UserProxyAgent(
+    name="user",
+    human_input_mode="TERMINATE",  
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": True,
+    },
+)
+
+user_proxy.initiate_chat(
+    assistant, message="Plot a chart of META and TESLA stock price change. Install any necessary dependencies"
+)
