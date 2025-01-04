76acda04e73a41a7d2b45339d6e55bbaf024635c
iaghidro <iaghidro@gmail.com>
2025-01-03 21:33:22 -0500

    add successful group chat using autogen 0.4

---
+++ group_chat.py
@@ -0,0 +1,40 @@
+import tempfile
+import os
+import asyncio
+from dataclasses import dataclass
+
+from autogen_core import CancellationToken
+from autogen_agentchat.messages import TextMessage
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_agentchat.agents import AssistantAgent, CodeExecutorAgent
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.ui import Console
+
+from dotenv import load_dotenv
+
+async def run_group_chat() -> None:
+    # Create a code executor agent that uses a Docker container to execute code.
+    code_executor = DockerCommandLineCodeExecutor(work_dir="coding")
+    await code_executor.start()
+    code_executor_agent = CodeExecutorAgent("code_executor", code_executor=code_executor)
+    
+    model_client = OpenAIChatCompletionClient(
+        model="o1-mini",
+        api_key=os.environ.get("OPENAI_API_KEY")
+    )
+    # The system message is not supported by the o1 series model.
+    assistantAgent = AssistantAgent(name="assistant", model_client=model_client, system_message=None)
+    
+    # Define termination condition
+    termination = TextMentionTermination("TERMINATE")
+    
+    # Define a team
+    agent_team = RoundRobinGroupChat([code_executor_agent,assistantAgent], termination_condition=termination, max_turns=30)
+
+    # Run the team and stream messages to the console
+    stream = agent_team.run_stream(task="Plot a chart of META and TESLA stock price change. Install any necessary dependencies")
+    await Console(stream)
+
+asyncio.run(run_group_chat())
