b0a625d1b5892317e2d8700a013ef69502a19e32
iaghidro <iaghidro@gmail.com>
2025-01-06 22:05:10 -0500

    minor refactor to be explicit about inputs to coding agent

--- group_chat_tool.py
+++ group_chat_tool.py
@@ -2,12 +2,18 @@ import asyncio
 from autogen_agentchat.conditions import HandoffTermination
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.ui import Console
+from shared.model_client import OpenAIModel, create_model_client
+from autogen_agentchat.base import Handoff
 
 from shared.executors import CodingAgent
 
 async def main():
 
-    coding_agent = CodingAgent().get_agent()
+    coding_agent = CodingAgent(
+        name="assistant",
+        model_client=create_model_client(OpenAIModel.GPT_4O_MINI),
+        handoffs=[Handoff(target="user", message="Transfer to user.")]
+        ).get_agent()
 
     team = RoundRobinGroupChat(
         participants=[coding_agent],
